9c4f54c6cb27e34ba3da3a72acac3740fb01f28a
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-16 09:18:57 -0500

    Meme maker LibreJS

--- tools/mememaker/script.js
+++ tools/mememaker/script.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 console.log("OwO what's this")
 function updateTop() {
     var topInput = document.getElementById("topInput");
@@ -16,3 +17,4 @@ function updateImg() {
     var file = document.querySelector('input[type=file]').files[0];
     img.src = window.URL.createObjectURL(file);
 }
+// @license-end
