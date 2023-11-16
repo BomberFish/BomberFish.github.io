310d331f4b3194c944093f755355962ac1697eae
BomberFish <bomberfish@bomberfish.ca>
2023-11-16 10:30:50 -0500

    seperate games

--- games/drift-hunters/js/start.js
+++
@@ -1,4 +0,0 @@
-var gameInstance = UnityLoader.instantiate("game", "./unity/drift_hunters.json", {onProgress: UnityProgress});
-document.getElementsByClassName('fullscreen')[0].addEventListener('click', () => {
-  gameInstance.SetFullscreen(1)
-});
