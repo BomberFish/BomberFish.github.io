51a706fe242d311f1b400d1f6af23df6c957fc40
BomberFish <bomberfish@bomberfish.ca>
2024-03-15 08:55:36 -0400

    make it look nice

---
+++ script.js
@@ -0,0 +1,5 @@
+var rotate = 0;
+document.getElementById("logo").addEventListener("click", function () {
+  rotate += 1440;
+  document.getElementById("logo").style.transform = "rotate(" + rotate + "deg)";
+});
