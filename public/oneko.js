e6287327492302fd327480b7642fe611d7d8f70b
BomberFish <bomberfish@bomberfish.ca>
2024-05-07 22:08:32 -0400

    oneko changes

--- public/oneko.js
+++ public/oneko.js
@@ -1,6 +1,42 @@
 // oneko.js: https://github.com/adryd325/oneko.js
 
+const mobileRE = /(android|bb\d+|meego).+mobile|armv7l|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|samsungbrowser.*mobile|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i
+const notMobileRE = /CrOS/
+
+const tabletRE = /android|ipad|playbook|silk/i
+
+function isMobile (opts) {
+  if (!opts) opts = {}
+  let ua = opts.ua
+  if (!ua && typeof navigator !== 'undefined') ua = navigator.userAgent
+  if (ua && ua.headers && typeof ua.headers['user-agent'] === 'string') {
+    ua = ua.headers['user-agent']
+  }
+  if (typeof ua !== 'string') return false
+
+  let result =
+    (mobileRE.test(ua) && !notMobileRE.test(ua)) ||
+    (!!opts.tablet && tabletRE.test(ua))
+
+  if (
+    !result &&
+    opts.tablet &&
+    opts.featureDetect &&
+    navigator &&
+    navigator.maxTouchPoints > 1 &&
+    ua.indexOf('Macintosh') !== -1 &&
+    ua.indexOf('Safari') !== -1
+  ) {
+    result = true
+  }
+
+  return result
+}
+
 (function oneko() {
+    if (isMobile({tablet: true})) {
+        return
+    }
     console.log("ONEKO INIT")
     const isReducedMotion =
       window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
@@ -15,11 +51,12 @@
     }
 
     let i = convertRemToPixels(3.5)
+    let j = convertRemToPixels(2)
 
-    let nekoPosX = i;
+    let nekoPosX = i + j;
     let nekoPosY = i + 32;
   
-    let mousePosX = i;
+    let mousePosX = i + j;
     let mousePosY = i + 32;
   
     let frameCount = 0;
